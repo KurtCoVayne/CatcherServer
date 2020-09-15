0f2ae7b4783d88e86cec8d35b631b9e39e87c8c4
John Alejandro Gonzalez <johnalejandrog.g4@gmail.com>
2020-09-15 10:05:07 -0500

    Added support for other methods and tokens

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,30 @@ router.get('*', function (req, res, next) {
   console.log('headers', req.headers)
   console.log('params', req.params)
   console.log('body', req.body)
-  res.json('Received')
+  res.json({"success":true,token:"null"})
 });
 router.post('*', function (req, res, next) {
   console.log('headers', req.headers)
   console.log('params', req.params)
   console.log('body', req.body)
-  res.json('Received')
+  res.json({"success":true,token:"null"})
 });
-
+router.put('*', function (req, res, next) {
+  console.log('headers', req.headers)
+  console.log('params', req.params)
+  console.log('body', req.body)
+  res.json({"success":true,token:"null"})
+});
+router.patch('*', function (req, res, next) {
+  console.log('headers', req.headers)
+  console.log('params', req.params)
+  console.log('body', req.body)
+  res.json({"success":true,token:"null"})
+});
+router.delete('*', function (req, res, next) {
+  console.log('headers', req.headers)
+  console.log('params', req.params)
+  console.log('body', req.body)
+  res.json({"success":true,token:"null"})
+});
 module.exports = router;
